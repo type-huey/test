f3fad8a94de73e17a2530e665032ccc240e43871
Sanghun Han <sanghun33@naver.com>
2023-07-26 09:42:46 +0900

    main-commit2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 let isDisableKeepAlive = false
+
 app.use(function(req, res, next) {
   if (isDisableKeepAlive) {
     res.set('Connection', 'close')
