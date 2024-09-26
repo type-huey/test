fea09d359b21e4d88431de0645138b1ad15c94ce
Sanghun Han <sanghun33@naver.com>
2024-09-26 10:53:35 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -8,19 +8,6 @@ export default defineConfig({
   server: {
     open: true,
     port: 3000,
-    proxy: {
-      '/redmine/': {
-        target: 'https://projects.rsupport.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/redmine\//, ''),
-        secure: true,
-      },
-      '/api': {
-        target: 'https://ai.rsup.io',
-        changeOrigin: true,
-        secure: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+    
   },
 });
